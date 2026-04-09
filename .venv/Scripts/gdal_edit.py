2dae40d86f559d26f7ed9b499a3881d89a6e8743
Kaden Francis McCulloch <kfmcculloch@arizona.edu>
2026-04-09 14:11:12 -0700

    added gis example: tucson landuse change

---
+++ .venv/Scripts/gdal_edit.py
@@ -0,0 +1,13 @@
+#!C:\Python\Personal\mcculloch-processors\.venv\Scripts\python.exe
+
+
+import sys
+
+from osgeo.gdal import deprecation_warn
+
+# import osgeo_utils.gdal_edit as a convenience to use as a script
+from osgeo_utils.gdal_edit import *  # noqa
+from osgeo_utils.gdal_edit import main
+
+deprecation_warn("gdal_edit")
+sys.exit(main(sys.argv))
---
+++ .venv/Scripts/gdalattachpct.py
@@ -0,0 +1,13 @@
+#!C:\Python\Personal\mcculloch-processors\.venv\Scripts\python.exe
+
+
+import sys
+
+from osgeo.gdal import deprecation_warn
+
+# import osgeo_utils.gdalattachpct as a convenience to use as a script
+from osgeo_utils.gdalattachpct import *  # noqa
+from osgeo_utils.gdalattachpct import main
+
+deprecation_warn("gdalattachpct")
+sys.exit(main(sys.argv))
